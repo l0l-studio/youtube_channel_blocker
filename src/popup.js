712904cd88cb6887cbe702bb181bfeec17dfa617
xanderjakeq <xanderjakeq@gmail.com>
2022-05-12 21:19:35 -0400

    refactor: tab style

--- src/popup.js
+++ src/popup.js
@@ -3,25 +3,25 @@ const block_button = document.getElementById('block');
 const reset = document.getElementById('reset');
 
 const options = {
-  type: 'basic',
-  title: 'Channel Blocker',
-  iconUrl: './icon.png',
+    type: 'basic',
+    title: 'Channel Blocker',
+    iconUrl: './icon.png',
 };
 
 block_button.addEventListener('click', () => {
-  chrome.runtime.sendMessage({
-    msg: 'block_channel',
-    channel: channel_input.value,
-  });
+    chrome.runtime.sendMessage({
+        msg: 'block_channel',
+        channel: channel_input.value,
+    });
 
-  chrome.notifications.create({
-    ...options,
-    message: `Blocked ${channel.value}`,
-  });
-  channel_input.value = '';
+    chrome.notifications.create({
+        ...options,
+        message: `Blocked ${channel.value}`,
+    });
+    channel_input.value = '';
 });
 
 reset.addEventListener('click', () => {
-  chrome.runtime.sendMessage({ msg: 'reset' });
-  chrome.notifications.create({ ...options, message: 'Reset successful' });
+    chrome.runtime.sendMessage({ msg: 'reset' });
+    chrome.notifications.create({ ...options, message: 'Reset successful' });
 });
