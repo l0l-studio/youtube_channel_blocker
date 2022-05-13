459a5d8545ab0a2d05fc9a97ed99074dacefaeef
xanderjakeq <xanderjakeq@gmail.com>
2022-05-12 21:17:14 -0400

    fix: inconsistent loading of blocked channels

--- src/background.js
+++ src/background.js
@@ -1,62 +1,77 @@
 let channels = new Set();
-chrome.storage.sync.get(['channels'], (res) => {
-  if (res.channels) {
-    for (const channel of JSON.parse(res.channels)) {
-      channels.add(channel);
-    }
-  }
-});
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  switch (request.msg) {
-    case 'page_load':
-      sendResponse({
-        channels: set_to_array(channels),
-      });
-      break;
-
-    case 'block_channel':
-      debugger;
-      if (!channels.has(request.channel)) {
-        channels.add(request.channel);
-
-        chrome.storage.sync.set({
-          channels: JSON.stringify(set_to_array(channels)),
-        });
-
-        chrome.tabs.query({}, function (tabs) {
-          const data = {
-            message: 'updated_channels',
-            channel: request.channel,
-          };
-
-          for (let i = 0; i < tabs.length; i++) {
-            chrome.tabs.sendMessage(tabs[i].id, data);
-          }
-        });
-      }
-      break;
-
-    case 'reset':
-      chrome.storage.sync.clear();
-      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        const data = {
-          message: 'reset',
-        };
-
-        for (let i = 0; i < tabs.length; i++) {
-          chrome.tabs.sendMessage(tabs[i].id, data);
+    switch (request.msg) {
+        case 'page_load': {
+            if (channels.size > 0) {
+                sendResponse({
+                    channels: set_to_array(channels),
+                });
+            } else {
+                get_data(sendResponse);
+            }
+            break;
         }
-      });
-      sendResponse();
-      break;
 
-    default:
-      throw 'action not supported';
-  }
-  return true;
+        case 'block_channel': {
+            if (!channels.has(request.channel)) {
+                channels.add(request.channel);
+
+                chrome.storage.local.set({
+                    //channels: JSON.stringify(set_to_array(channels)),
+                    //channels: set_to_array(channels),
+                    [request.channel]: '',
+                });
+
+                chrome.tabs.query({}, function(tabs) {
+                    const data = {
+                        message: 'updated_channels',
+                        channel: request.channel,
+                    };
+
+                    for (let i = 0; i < tabs.length; i++) {
+                        chrome.tabs.sendMessage(tabs[i].id, data);
+                    }
+                });
+            }
+            break;
+        }
+
+        case 'reset': {
+            chrome.storage.local.clear();
+            chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+                const data = {
+                    message: 'reset',
+                };
+
+                for (let i = 0; i < tabs.length; i++) {
+                    chrome.tabs.sendMessage(tabs[i].id, data);
+                }
+            });
+            sendResponse();
+            break;
+        }
+
+        default: {
+            throw 'action not supported';
+        }
+    }
+    return true;
 });
 
+const get_data = (sendResponse) => {
+    chrome.storage.local.get(null).then((results) => {
+        const blocked_channels = Object.keys(results);
+        for (let channel of blocked_channels) {
+            channels.add(channel);
+        }
+    }, on_error);
+};
+
 const set_to_array = (set) => {
-  return [...set];
+    return [...set];
+};
+
+const on_error = (e) => {
+    console.log(e);
 };
--- src/content.js
+++ src/content.js
@@ -1,77 +1,86 @@
 /*----------------------------
 GLOBALS
 ----------------------------*/
-let data = new Set();
+let data = null;
 let should_reload = false;
 
 const thumbnail_tags = {
-  main: 'ytd-rich-item-renderer',
-  watch: 'ytd-compact-video-renderer',
+    main: 'ytd-rich-item-renderer',
+    watch: 'ytd-compact-video-renderer',
 };
 
 const channel_name_element = 'yt-formatted-string';
 
 window.onload = () => {
-  const contents = document.getElementById('page-manager');
-  const secondary = document.getElementById('secondary');
+    const contents = document.getElementById('page-manager');
+    const secondary = document.getElementById('secondary');
 
-  const observer_0 = new MutationObserver(handle_mutation);
-  const observer_1 = new MutationObserver(handle_mutation);
+    const observer_0 = new MutationObserver(handle_mutation);
+    const observer_1 = new MutationObserver(handle_mutation);
 
-  observer_0.observe(contents, { childList: true, subtree: true });
-  if (secondary) {
-    observer_1.observe(secondary, { childList: true, subtree: true });
-  }
+    observer_0.observe(contents, { childList: true, subtree: true });
+    if (secondary) {
+        observer_1.observe(secondary, { childList: true, subtree: true });
+    }
 
-  window.onbeforeunload = () => {
-    observer_0.disconnect();
-    observer_1.disconnect();
-  };
+    window.onbeforeunload = () => {
+        observer_0.disconnect();
+        observer_1.disconnect();
+    };
 
-  chrome.runtime.sendMessage({ msg: 'page_load' }, (response) => {
-    data = new Set(response.channels);
-    remove_features(data);
-  });
+    let timeout;
+    timeout = setInterval(() => {
+        if (data) {
+            clearTimeout(timeout);
+            return;
+        }
+
+        chrome.runtime.sendMessage({ msg: 'page_load' }, (response) => {
+            data = new Set(response.channels);
+            console.log(data);
+            remove_features(data);
+        });
+    });
 };
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  switch (request.message) {
-    case 'updated_channels':
-      data.add(request.channel);
-      remove_features(data);
-      break;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    switch (request.message) {
+        case 'updated_channels':
+            data.add(request.channel);
+            remove_features(data);
+            break;
 
-    case 'reset':
-      data.clear();
-      break;
+        case 'reset':
+            data.clear();
+            break;
 
-    default:
-      throw 'event does not exist';
-  }
+        default:
+            throw 'event does not exist';
+    }
 
-  sendResponse();
+    sendResponse();
 });
 
 const handle_mutation = (mutation_list, observer) => {
-  //TODO: use the mutation_list to only process new elements
-  remove_features(data);
+    //TODO: use the mutation_list to only process new elements
+    remove_features(data);
 };
 
 const remove_features = (data) => {
-  const page = !window.location.href.match('watch') ? 'main' : 'watch';
-  const videos = document.getElementsByTagName(thumbnail_tags[page]);
+    const page = !window.location.href.match('watch') ? 'main' : 'watch';
+    const videos = document.getElementsByTagName(thumbnail_tags[page]);
 
-  for (let i = 0; i < videos.length; i++) {
-    const cn_containers = videos[i].getElementsByTagName(channel_name_element);
+    for (let i = 0; i < videos.length; i++) {
+        const cn_containers = videos[i].getElementsByTagName(channel_name_element);
 
-    if (cn_containers.length > 0) {
-      //watch page [0] cause there's only 1 'yt-formatted-string'
-      //main page [1] cause there's 2 'yt-formatted-string'
-      const channel_name = page === 'watch' ? 0 : 1;
+        if (cn_containers.length > 0) {
+            //watch page [0] cause there's only 1 'yt-formatted-string'
+            //main page [1] cause there's 2 'yt-formatted-string'
+            const channel_name = page === 'watch' ? 0 : 1;
 
-      if (data.has(cn_containers[channel_name].innerText)) {
-        videos[i].remove();
-      }
+            if (data.has(cn_containers[channel_name].innerText)) {
+                videos[i].remove();
+            }
+        }
     }
-  }
 };
